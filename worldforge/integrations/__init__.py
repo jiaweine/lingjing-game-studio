9b0b863fbe194a7a786e3018f022cc388deeb762
jwya <1817219421@qq.com>
2026-08-20 18:08:55 +0800

    feat: export game execution contracts

--- worldforge/integrations/__init__.py
+++ worldforge/integrations/__init__.py
@@ -1,1 +1,24 @@
+from .game_execution import (
+    EvidenceProvenance,
+    ExecutionAction,
+    ExecutionCheckpoint,
+    ExecutionObservation,
+    GameBuildRef,
+    GameExecutionAdapter,
+    GameExecutionAdapterRegistry,
+    VerificationResult,
+)
 from .mcp_adapter import MCPGameAdapter, ToolSpec
+
+__all__ = [
+    "EvidenceProvenance",
+    "ExecutionAction",
+    "ExecutionCheckpoint",
+    "ExecutionObservation",
+    "GameBuildRef",
+    "GameExecutionAdapter",
+    "GameExecutionAdapterRegistry",
+    "MCPGameAdapter",
+    "ToolSpec",
+    "VerificationResult",
+]
