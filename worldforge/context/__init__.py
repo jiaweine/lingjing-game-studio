67161169a1d1f2ec1326d8539e67dcf83fb4ad72
jwya <1817219421@qq.com>
2026-09-05 02:11:35 +0800

    feat: export multimodal context compiler

--- worldforge/context/__init__.py
+++ worldforge/context/__init__.py
@@ -1,3 +1,9 @@
 from .compiler import ContextCompiler, ContextPacket
+from .multimodal import MultimodalContextCompiler, MultimodalPacket
 
-__all__ = ["ContextCompiler", "ContextPacket"]
+__all__ = [
+    "ContextCompiler",
+    "ContextPacket",
+    "MultimodalContextCompiler",
+    "MultimodalPacket",
+]
