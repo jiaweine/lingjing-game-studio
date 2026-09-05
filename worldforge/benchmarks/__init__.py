b3d67aaf5e7e4f046546c88a0f3ce5bbd37174ff
jwya <1817219421@qq.com>
2026-09-05 15:50:27 +0800

    feat(benchmark): export ContextOS benchmark

--- worldforge/benchmarks/__init__.py
+++ worldforge/benchmarks/__init__.py
@@ -1,1 +1,9 @@
+from .context_eval import ContextBenchmarkResult, run_context_benchmark
 from .game_eval import run_benchmark, run_episode
+
+__all__ = [
+    "ContextBenchmarkResult",
+    "run_context_benchmark",
+    "run_benchmark",
+    "run_episode",
+]
