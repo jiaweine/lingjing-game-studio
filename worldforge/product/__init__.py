bb664ed9ef54bfdb5647ace1c09697afda4c2c00
jwya <1817219421@qq.com>
2026-09-07 04:04:32 +0800

    fix: keep stable analyzer export while retaining scope safety

--- worldforge/product/__init__.py
+++ worldforge/product/__init__.py
@@ -1,6 +1,6 @@
 from .store import ConversationStore
 from .media import extract_video_frames, extract_video_keyframes, probe_media
-from .contextual_analyzer_v4 import ProductAnalyzer
+from .contextual_analyzer_v3 import ProductAnalyzer
 
 __all__ = [
     "ConversationStore",
