a36f88ee6b001594a2513fb46c6aa2bc2299c73e
jwya <1817219421@qq.com>
2026-09-07 00:28:53 +0800

    feat: export project memory benchmark

--- worldforge/benchmarks/__init__.py
+++ worldforge/benchmarks/__init__.py
@@ -1,9 +1,12 @@
 from .context_eval import ContextBenchmarkResult, run_context_benchmark
 from .game_eval import run_benchmark, run_episode
+from .memory_eval import MemoryBenchmarkResult, run_memory_benchmark
 
 __all__ = [
     "ContextBenchmarkResult",
     "run_context_benchmark",
+    "MemoryBenchmarkResult",
+    "run_memory_benchmark",
     "run_benchmark",
     "run_episode",
 ]
