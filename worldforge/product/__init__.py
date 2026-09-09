823452be4b6ed56c3b6e01ae7d4d9b6417da99e0
jwya <1817219421@qq.com>
2026-09-09 12:06:58 +0800

    fix: install product schema invariants at package load

--- worldforge/product/__init__.py
+++ worldforge/product/__init__.py
@@ -1,4 +1,8 @@
 from .store import ConversationStore
+from .schema_invariants import install_product_schema_invariants
+
+install_product_schema_invariants()
+
 from .media import extract_video_frames, extract_video_keyframes, probe_media
 from .contextual_analyzer_v3 import ProductAnalyzer
 
