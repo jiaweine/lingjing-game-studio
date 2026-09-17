40497243b4de7b3e7d0fa165c5fa504bd017ed06
jwya <1817219421@qq.com>
2026-09-17 23:31:15 +0800

    feat: expose value-metrics conversation store

--- worldforge/product/__init__.py
+++ worldforge/product/__init__.py
@@ -1,4 +1,4 @@
-from .store import ConversationStore
+from .value_store import ConversationStore
 from .media import extract_video_frames, extract_video_keyframes, probe_media
 from .contextual_analyzer_v3 import ProductAnalyzer
 
