b491a13a102ef5812b6fb4895aa7639d4c85618a
jwya <1817219421@qq.com>
2026-09-18 03:24:08 +0800

    Use GitHub CI trigger-aware conversation store

--- worldforge/product/__init__.py
+++ worldforge/product/__init__.py
@@ -1,4 +1,4 @@
-from .github_context_store import ConversationStore
+from .github_ci_trigger_store import ConversationStore
 from .media import extract_video_frames, extract_video_keyframes, probe_media
 from .contextual_analyzer_v3 import ProductAnalyzer
 
